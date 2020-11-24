7bc92e259dfa3444c73a77656bcfce9a6c5b66c9
Erin Matheny <erin_m713@yahoo.com>
2020-11-23 18:42:01 -0600

    commented out const cors

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 
-const cors = require("cors");
-app.use(cors());
+// const cors = require("cors");
+// app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
