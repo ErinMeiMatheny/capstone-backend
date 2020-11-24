28732a59586585383742245457189870d03ff1b1
Erin Matheny <erin_m713@yahoo.com>
2020-11-23 18:54:11 -0600

    changed header

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 
-// const cors = require("cors");
-// app.use(cors());
+const cors = require("cors");
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  next();
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
 });
 
 // const expressSession = require("express-session");
