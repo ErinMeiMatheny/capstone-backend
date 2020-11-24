ac7d440558f4ffb98a8e8c6aa95bcbb685ad5a3d
Erin Matheny <erin_m713@yahoo.com>
2020-11-23 18:39:50 -0600

    added http

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   next();
 });
 
