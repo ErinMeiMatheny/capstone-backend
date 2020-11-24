549273ddc47f7bd9850eda0653e62f74af98b216
Erin Matheny <erin_m713@yahoo.com>
2020-11-23 18:37:21 -0600

    changed header to localhost

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'localhost:3000');
   next();
 });
 
